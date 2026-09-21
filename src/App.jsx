4b230286dcc14644a80117cc87303e2f9323e41d
Mani <manikandan.rordev@gmail.com>
2026-09-21 11:56:38 +0530

    feat: add genre filter with useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import MovieCard from "./components/MovieCard";
 import Watchlist from "./components/Watchlist";
+import GenreFilter from "./components/GenreFilter";
 
 function App() {
   const [movies, setMovies] = useState([]);
+
+  const [selectedGenre, setSelectedGenre] = useState("All");
+
+  const genres = ["All", ...new Set(movies.map((m) => m.genre))];
+  const filteredMovies = selectedGenre === "All" ? movies : movies.filter((m) => m.genre === selectedGenre);
 
   useEffect(() => {
     axios.get("/movies.json").then((res) => setMovies(res.data));
@@ -26,13 +32,14 @@ function App() {
     <div className="container py-4">
       <h1 className="mb-4 text-center">🎬 Movie Watchlist</h1>
       <div className="row">
+        <GenreFilter genres={genres} selectedGenre={selectedGenre} onSelect={setSelectedGenre} />
         <div className="col-lg-8">
           <div className="row g-3">
-          {movies.map((movie) => (
-            <div className="col-sm-6 col-md-4" key={movie.id}>
-              <MovieCard title={movie.title} genre={movie.genre} year={movie.year} rating={movie.rating} language={movie.language} isAdded={watchlist.some((m) => m.id === movie.id)} onAdd={() => addToWatchlist(movie)} />
-            </div>
-          ))}
+            {movies.map((movie) => (
+              <div className="col-sm-6 col-md-4" key={movie.id}>
+                <MovieCard title={movie.title} genre={movie.genre} year={movie.year} rating={movie.rating} language={movie.language} isAdded={watchlist.some((m) => m.id === movie.id)} onAdd={() => addToWatchlist(movie)} />
+              </div>
+            ))}
           </div>
         </div>
         <div className="col-lg-4 mt-4 mt-lg-0">
--- src/components/GenreFilter.jsx
+++ src/components/GenreFilter.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
-function GenreFilter() {
+function GenreFilter({ genres, selectedGenre, onSelect }) {
   return (
-    <div>GenreFilter</div>
-  )
+    <div className="mb-4 d-flex flex-wrap gap-2">
+      {genres.map((genre) => (
+        <button
+          key={genre}
+          className={`btn ${
+            selectedGenre === genre ? "btn-dark" : "btn-outline-dark"
+          }`}
+          onClick={() => onSelect(genre)}
+        >
+          {genre}
+        </button>
+      ))}
+    </div>
+  );
 }
 
-export default GenreFilter
+export default GenreFilter;
